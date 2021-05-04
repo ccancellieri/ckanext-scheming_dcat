962802a4b770a5087ec517b60e1f9b25e9532e62
ccancellieri <carlo.cancellieri@fao.org>
2021-05-04 19:05:26 +0200

    first public release

---
+++ ckanext/scheming_dcat/plugin.py
@@ -0,0 +1,13 @@
+from exceptions import Exception
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class SchemingDcatPlugin(plugins.SingletonPlugin,toolkit.DefaultDatasetForm):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'scheming_dcat')
